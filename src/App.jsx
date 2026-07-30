c281ae3e910d86d08f8e7773e5bebc857a086c92
vishnu2815 <vishnumathariya28@gmail.com>
2026-07-30 19:29:18 +0530

    Remove unused code form App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,10 @@
-import { CounterWithCallback } from "./useCallbackQues/BasicCounter.jsx";
-import { TextUpdater } from "./useCallbackQues/TextUpdater.jsx";
-import { UserList } from "./useCallbackQues/UserList.jsx";
-import { SearchCommentsByEmail } from "./useEffectQues/SearchCommentsByEmail.jsx";
-import { SearchPostsByBody } from "./useEffectQues/searchPostsByBody.jsx";
-import { SearchPostsByTitle1 } from "./useEffectQues/SearchPostsByTitle1.jsx";
-import { SearchTodosByUserId } from "./useEffectQues/SearchTodosByUserId.jsx";
-import { SearchUserByName } from "./useEffectQues/searchUserByName.jsx";
-import { SearchUsersByCompanyName } from "./useEffectQues/SearchUsersByCompanyName.jsx";
-import { CalculateTotalPrice } from "./useMemoQues/CalculateTotalPrice.jsx";
-import { CharacterCounter } from "./useMemoQues/CharacterCounter.jsx";
-import { EvenNumberCounter } from "./useMemoQues/EvenNumberCounter.jsx";
-import { EvenNumberFilter } from "./useMemoQues/EvenNumberFilter.jsx";
-import { ExpensiveProductCounter } from "./useMemoQues/ExpensiveProductCounter.jsx";
-import { ProductSearch } from "./useMemoQues/ProductSearch.jsx";
-import { ReverseText } from "./useMemoQues/ReverseText.jsx";
-import { SearchUsers } from "./useMemoQues/SearchUsers.jsx";
-import { SortProductsByPrice } from "./useMemoQues/SortProductsByPrice.jsx";
-import { UppercaseConverter } from "./useMemoQues/UppercaseConverter.jsx";
-import { VowelCounter } from "./useMemoQues/VowelCounter.jsx";
-import { WordCounter } from "./useMemoQues/WordCounter.jsx";
+import RegisterForm from "./project/registerForm";
 
 function App() {
   return (
-    <>
-      {/* <SearchUserByName/> */}
-      {/* <SearchPostsByBody/> */}
-      {/* <SearchCommentsByEmail/> */}
-      {/* <SearchUsersByCompanyName/> */}
-
-      {/* <SearchTodosByUserId/> */}
-      {/* <SearchPostsByTitle1/> */}
-      {/* <CalculateTotalPrice /> */}
-      {/* <EvenNumberCounter/> */}
-
-      {/* <UppercaseConverter/> */}
-
-      {/* <CharacterCounter/> */}
-
-      {/* <WordCounter /> */}
-      {/* <ReverseText /> */}
-      {/* <VowelCounter/> */}
-      {/* <EvenNumberFilter/> */}
-
-      {/* <SearchUsers/> */}
-
-      {/* <ProductSearch/> */}
-
-      {/* <ExpensiveProductCounter/> */}
-
-      {/* <SortProductsByPrice/> */}
-
-      {/* <CounterWithCallback/> */}
-
-      {/* <TextUpdater/> */}
-
-      <UserList/>
-
-    </>
+  <>
+  <RegisterForm/>
+  </>
   );
 }
 
